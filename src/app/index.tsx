f6e4c82dc745a8f40a02dc3eca306c139d22f08a
Valenttin Motter <valenttin@myscrumhalf.com>
2024-12-11 21:11:46 -0300

    aula 1

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { Steps } from "@/components/Steps";
+import { Welcome } from "@/components/Welcome";
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+      <Welcome />
+      <Steps />
+    </View>
+  );
+}
